85b6699a50d4e1041384dbc3e023005abe26c16a
Krati Mittal <krati@nickelfox.co>
2023-05-19 00:17:24 +0530

    first commit

---
+++ SwiftUIToDoList/SwiftUIToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToDoListApp.swift
+//  SwiftUIToDoList
+//
+//  Created by Krati Mittal on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
